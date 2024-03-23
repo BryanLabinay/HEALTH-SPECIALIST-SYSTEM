b9fcdb6a8c344351718a78be930c1393d93b12ee
SmokeCode00 <148770806+Smokecode00@users.noreply.github.com>
2024-03-23 20:34:35 +0800

    Setup Livewire Breeze

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,16 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::view('admin', 'admin')
+    ->middleware(['auth', 'verified'])
+    ->name('admin');
+
+Route::view('doctor', 'doctor')
+    ->middleware(['auth', 'verified'])
+    ->name('doctor');
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
